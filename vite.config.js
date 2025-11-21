4287a1eae01dfd425a8851bf99b6cf2a9f95b0dc
Wen <frondairwen@gmail.com>
2025-11-22 02:37:57 +0800

    vite config added base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/Invex",
   server: {
     proxy: {
       // This forwards all /api requests to Flask
