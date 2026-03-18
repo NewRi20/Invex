152786cc6137f4545c37bfeb92a60dd7680176d1
Wen <frondairwen@gmail.com>
2026-03-18 22:55:31 +0800

    Downloaded tailwind for the app

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 // In invex-app/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: "/",
   server: {
     proxy: {
